175dbc889799311abc2a7f7c04459f7ee29d4fde
swopnil420@gmail.com <swopnil420@gmail.com>
2016-07-19 20:56:00 +0600

    first commit

---
+++ bingo1/main.js
@@ -0,0 +1,7 @@
+window.onload = newCard;
+function newCard() {
+    for(var i = 0; i < 24; i++) {
+        var newNum = Math.floor(Math.random() * 75) + 1;
+        document.getElementById("square" + i).innerHTML = newNum;
+    }
+}
